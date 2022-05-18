9ba974099e8b670d9a9d1ca1b9628e0a3ef3689f
Aleksa Bubanja <aleksa3b@gmail.com>
2022-05-18 23:21:58 +0200

    Updates

---
+++ precache-manifest.ff23a8d9a3c07010b8323ff894564de6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "31d02aff7c8acfb67c3a",
+    "url": "/Travel-advisor/static/css/main.85077fd8.chunk.css"
+  },
+  {
+    "revision": "31d02aff7c8acfb67c3a",
+    "url": "/Travel-advisor/static/js/main.31d02aff.chunk.js"
+  },
+  {
+    "revision": "521dee7d6a1a5478516b",
+    "url": "/Travel-advisor/static/js/1.521dee7d.chunk.js"
+  },
+  {
+    "revision": "fe9ca1ba5eb3be9ae27f",
+    "url": "/Travel-advisor/static/js/2.fe9ca1ba.chunk.js"
+  },
+  {
+    "revision": "02e8bdd20abac1e46d76",
+    "url": "/Travel-advisor/static/js/runtime~main.02e8bdd2.js"
+  },
+  {
+    "revision": "4c6ed539e5f47e3f5388c0bdc97eb878",
+    "url": "/Travel-advisor/index.html"
+  }
+];
