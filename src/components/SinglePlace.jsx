6d5bf8b7cd1e689a99b79a514c3830221ae1d6e1
Aleksa Bubanja <aleksa3b@gmail.com>
2022-05-31 23:00:30 +0200

    SinglePlace component

--- src/components/SinglePlace.jsx
+++ src/components/SinglePlace.jsx
@@ -3,7 +3,47 @@ import React from "react";
 function SinglePlace({ place }) {
   console.log(place);
 
-  return <div>SinglePlace</div>;
+  return (
+    <section>
+      <img
+        src={
+          place.photo
+            ? place.photo.images.large.url
+            : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"
+        }
+        title={place.name}
+      />
+      <h5>{place.name}</h5>
+      <h2>{place.rating}</h2>
+      <h1>{place.price_level}</h1>
+      <h2>{place.ranking}</h2>
+      {place.awards.map((award) => (
+        <div>
+          <img src={award.images.small} />
+          <h1>{award.display_name}</h1>
+        </div>
+      ))}
+      {place.cuisine.map(({ name }) => (
+        <h1>{name}</h1>
+      ))}
+      {place.address && <h3>{place.address}</h3>}
+      {place.phone && <h1>{place.phone}</h1>}
+      <button
+        size="small"
+        color="primary"
+        onClick={() => window.open(place.web_url, "_blank")}
+      >
+        Trip Advisor
+      </button>
+      <button
+        size="small"
+        color="primary"
+        onClick={() => window.open(place.website, "_blank")}
+      >
+        Website
+      </button>
+    </section>
+  );
 }
 
 export default SinglePlace;
