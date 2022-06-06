20e79a110f2a9a2183143a3a6e182c0640001d57
Aleksa Bubanja <aleksa3b@gmail.com>
2022-06-06 21:34:14 +0200

    Updates

---
+++ precache-manifest.4f271afc28571a7eac70dc90566ce4a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ad2b61b043d116d4dc1",
+    "url": "/Travel-advisor/static/css/main.efc8ab50.chunk.css"
+  },
+  {
+    "revision": "0ad2b61b043d116d4dc1",
+    "url": "/Travel-advisor/static/js/main.57ef8f94.chunk.js"
+  },
+  {
+    "revision": "a883ea5026715ffc9047",
+    "url": "/Travel-advisor/static/js/runtime~main.3f9a60c7.js"
+  },
+  {
+    "revision": "5c44c5cbf78d4a3a841f",
+    "url": "/Travel-advisor/static/js/2.fee372f2.chunk.js"
+  },
+  {
+    "revision": "d1033c6ea6b1e82f2b86",
+    "url": "/Travel-advisor/static/js/3.68ba455b.chunk.js"
+  },
+  {
+    "revision": "b9ff35858f03d854d02bccc889d4fc78",
+    "url": "/Travel-advisor/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Travel-advisor/precache-manifest.9374ca70e896d749d1fa848f13f2e9af.js"
+  "/Travel-advisor/precache-manifest.4f271afc28571a7eac70dc90566ce4a5.js"
 );
 
 workbox.clientsClaim();
